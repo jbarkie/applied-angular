32b8bc598616a409683d25ef4f46276911becded
Joe Barkie <josephbarkie@hotmail.com>
2024-12-16 16:35:07 -0500

    Add route
    For news component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NewsComponent } from './areas/news/news.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'news',
+    component: NewsComponent,
+  },
+];
