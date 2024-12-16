38f5792e66f989181042093fd3f662a30bbad37c
Joe Barkie <josephbarkie@hotmail.com>
2024-12-16 12:35:11 -0500

    Break for lunch

--- src/app/areas/news/components/news-list.component.ts
+++ src/app/areas/news/components/news-list.component.ts
@@ -1,4 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+
+type NewArticle = {
+  id: string;
+  title: string;
+  shortDescription: string;
+  link: string;
+  datePublished: string;
+};
 
 @Component({
   selector: 'app-news-list',
@@ -6,17 +14,34 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   imports: [],
   template: `
     <section>
-      <article>
-        <p>It Rained Yesterday!</p>
-      </article>
-      <article>
-        <p>It Rained Yesterday!</p>
-      </article>
-      <article>
-        <p>It Rained Yesterday!</p>
-      </article>
+      @for (article of articles(); track article.id) {
+        <article class="card bg-base-100 shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title">{{ article.title }}</h2>
+            <p>{{ article.shortDescription }}</p>
+            <p>
+              <small>{{ article.datePublished }}</small>
+            </p>
+            <div class="card-actions justify-end">
+              <a class="btn btn-primary" href="">{{ article.link }}</a>
+            </div>
+          </div>
+        </article>
+      } @empty {
+        <p>No news! Check back later.</p>
+      }
     </section>
   `,
   styles: ``,
 })
-export class NewsListComponent {}
+export class NewsListComponent {
+  articles = signal<NewArticle[]>([
+    {
+      id: '1',
+      title: 'Lunchtime',
+      shortDescription: 'Time to eat',
+      link: 'https://mylink',
+      datePublished: '2024-12-16T17:32:12.556Z',
+    },
+  ]);
+}
