f6cf157f0305bafb4a11c24a76c5288121dc41cd
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 11:09:16 -0500

    Fetch articles resource from API call

--- src/app/areas/news/components/news-list.component.ts
+++ src/app/areas/news/components/news-list.component.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectionStrategy,
   signal,
   computed,
+  resource,
 } from '@angular/core';
 import { NewsArticle } from '../types';
 import { NewsItemComponent } from './news-item.component';
@@ -20,7 +21,7 @@ import { NewsItemComponent } from './news-item.component';
       <p>Read some stuff!</p>
     }
     <section>
-      @for (article of articles(); track article.id) {
+      @for (article of articles.value; track article.id) {
         <app-news-item
           (linkRead)="readTheArticle($event)"
           [articleToDisplay]="article"
@@ -35,34 +36,12 @@ import { NewsItemComponent } from './news-item.component';
 })
 export class NewsListComponent {
   preferredHeader = signal('Preferred header');
-  articles = signal<NewsArticle[]>([
-    {
-      id: '1',
-      title: 'Angular Inputs and Outputs',
-      shortDescription:
-        'Read about Angular inputs and outputs in "modern" Angular',
-      link: 'https://applied-angular.hypertheory.com/guides/components.html',
-      datePublished: '2024-12-16T17:32:12.556Z',
-      linkSlug: 'Read about Angular Inputs & Outputs',
-    },
-    {
-      id: '2',
-      title: 'Creating an Angular Application',
-      shortDescription: 'Learn how to create a super rad Angular app!',
-      link: 'https://applied-angular.hypertheory.com/guides/angular-setup.html',
-      datePublished: '2024-12-05T13:18:12.556Z',
-      linkSlug: 'Read about creating Angular apps',
-    },
-    {
-      id: '3',
-      title: 'Deborah Kurata YouTube',
-      shortDescription:
-        'Youtube Videos about Angular Signals and stuff from Deborah Kurata',
-      link: 'https://www.youtube.com/@deborah_kurata/videos',
-      datePublished: '2024-11-10T10:15:12.556Z',
-      linkSlug: 'Read about Angular Signals',
-    },
-  ]);
+  articles = resource<NewsArticle[], unknown>({
+    loader: () =>
+      fetch('http://fake-news.some-fake-server.com/news-feed').then((r) =>
+        r.json(),
+      ),
+  });
 
   readArticles = signal<NewsArticle[]>([]);
   readArticleCount = computed(() => this.readArticles().length);
