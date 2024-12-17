e1a55e5a096d5c0c44cf59ef8a98bc27e0d8dc3c
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 12:16:34 -0500

    Provide store
    Anywhere the NewsComponent is used.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { NewsComponent } from './areas/news/news.component';
+import { ReadArticleStore } from './areas/news/services/read-articles.store';
 
 export const routes: Routes = [
   {
     path: 'news',
+    providers: [ReadArticleStore],
     component: NewsComponent,
   },
 ];
