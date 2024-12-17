57a3ea5f6da48af4fcf442ae3feb4e28552e48a0
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 12:35:46 -0500

    Allow store to be viewed from Redux DevTools

--- src/app/areas/news/services/read-articles.store.ts
+++ src/app/areas/news/services/read-articles.store.ts
@@ -1,8 +1,10 @@
 import { patchState, signalStore, withMethods } from '@ngrx/signals';
 import { addEntity, withEntities } from '@ngrx/signals/entities';
 import { NewsArticle } from '../types';
+import { withDevtools } from '@angular-architects/ngrx-toolkit';
 
 export const ReadArticleStore = signalStore(
+  withDevtools('read-articles'),
   withEntities<NewsArticle>(),
   withMethods((store) => {
     // later
