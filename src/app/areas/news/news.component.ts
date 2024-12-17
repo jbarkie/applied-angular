76c6e8a3cf3a881f81ea209965ad21bf513ed1d4
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 14:42:03 -0500

    Update imports

--- src/app/areas/news/news.component.ts
+++ src/app/areas/news/news.component.ts
@@ -1,12 +1,11 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { NewsHeaderComponent } from './components/news-header.component';
-import { NewsListComponent } from './components/news-list.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NewsHeaderComponent } from './components/news-header.component';
 
 @Component({
   selector: 'app-news',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NewsHeaderComponent, NewsListComponent, RouterLink, RouterOutlet],
+  imports: [NewsHeaderComponent, RouterLink, RouterOutlet],
   template: `
     <app-news-header />
     <div class="flex gap-4">
