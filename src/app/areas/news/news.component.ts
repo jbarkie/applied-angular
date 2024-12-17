7f362ddf6bf83e1732506025801773fc10ab81bd
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 12:22:53 -0500

    Refactor component

--- src/app/areas/news/news.component.ts
+++ src/app/areas/news/news.component.ts
@@ -1,14 +1,19 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { NewsHeaderComponent } from './components/news-header.component';
 import { NewsListComponent } from './components/news-list.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-news',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [NewsHeaderComponent, NewsListComponent],
+  imports: [NewsHeaderComponent, NewsListComponent, RouterLink, RouterOutlet],
   template: `
     <app-news-header />
-    <app-news-list />
+    <div class="flex gap-4">
+      <a routerLink="list" class="link">List</a>
+      <a routerLink="history" class="link">History</a>
+    </div>
+    <router-outlet />
   `,
   styles: ``,
 })
