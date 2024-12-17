e7aa414708a37a60c24c823d742d390ce7cf06a0
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 12:24:31 -0500

    Add component

---
+++ src/app/areas/news/components/history.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-news-history',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>Put the history here</p> `,
+  styles: ``,
+})
+export class HistoryComponent {}
