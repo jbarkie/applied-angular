7d80461a894031a56e4520ee0824069ad322ee22
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 12:27:58 -0500

    Display history
    Using ReadArticleStore.

--- src/app/areas/news/components/history.component.ts
+++ src/app/areas/news/components/history.component.ts
@@ -1,10 +1,24 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { ReadArticleStore } from '../services/read-articles.store';
 
 @Component({
   selector: 'app-news-history',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` <p>Put the history here</p> `,
+  template: `
+    @if (store.entities().length === 0) {
+      <p>You've read no articles</p>
+    } @else {
+      <p>You've read the following articles:</p>
+      <ul>
+        @for (article of store.entities(); track article.id) {
+          <li>{{ article.title }}</li>
+        }
+      </ul>
+    }
+  `,
   styles: ``,
 })
-export class HistoryComponent {}
+export class HistoryComponent {
+  store = inject(ReadArticleStore);
+}
