5d8837bb4b418cca0fdc0295a8a33c4009de1622
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 12:24:44 -0500

    Update routes
    Add children paths.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
 import { NewsComponent } from './areas/news/news.component';
 import { ReadArticleStore } from './areas/news/services/read-articles.store';
+import { NewsListComponent } from './areas/news/components/news-list.component';
+import { HistoryComponent } from './areas/news/components/history.component';
 
 export const routes: Routes = [
   {
     path: 'news',
     providers: [ReadArticleStore],
     component: NewsComponent,
+    children: [
+      {
+        path: 'list',
+        component: NewsListComponent,
+      },
+      {
+        path: 'history',
+        component: HistoryComponent,
+      },
+    ],
   },
 ];
