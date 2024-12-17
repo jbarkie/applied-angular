4bd18f871fcb9625bff0cd5f9c13161c74eb3f57
Joe Barkie <josephbarkie@hotmail.com>
2024-12-17 10:57:21 -0500

    Move component
    And add optional suffix.

--- src/app/shared/components/relative-time.component.ts
+++ src/app/shared/components/relative-time.component.ts
@@ -8,7 +8,7 @@ import {
 import { formatDistanceToNow } from 'date-fns';
 
 @Component({
-  selector: 'app-news-relative-time',
+  selector: 'app-relative-time',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
   template: ` <span>{{ relativeDate() }}</span> `,
@@ -16,12 +16,13 @@ import { formatDistanceToNow } from 'date-fns';
 })
 export class RelativeTimeComponent {
   relativeDate = signal('');
+  suffix = signal(' ago');
   date = input.required<string>();
   constructor() {
     effect(() => {
       const intervalId = setInterval(() => {
         this.relativeDate.set(
-          formatDistanceToNow(new Date(this.date())) + ' ago',
+          formatDistanceToNow(new Date(this.date())) + this.suffix(),
         );
       }, 1000);
       return () => {
