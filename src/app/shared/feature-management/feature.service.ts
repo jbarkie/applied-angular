6f5d2d0614dd0c478f0e7b4407c3e57ca5457ac9
Joe Barkie <josephbarkie@hotmail.com>
2024-12-18 16:16:59 -0500

    Remove decorator
    Not needed.

--- src/app/shared/feature-management/feature.service.ts
+++ src/app/shared/feature-management/feature.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject } from '@angular/core';
 import { FEATURE_FLAG_URL } from '.';
 
-@Injectable({ providedIn: 'root' })
 export class FeaturesService {
   #client = inject(HttpClient);
 
