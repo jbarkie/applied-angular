8c1fa35953a51e41fa13e1ee5f32b7e488adb422
Joe Barkie <josephbarkie@hotmail.com>
2024-12-18 12:23:17 -0500

    Add feature management

---
+++ src/app/shared/feature-management/index.ts
@@ -0,0 +1,4 @@
+export const FEATURE_FLAG_URL = '/api/features';
+export * from './feature.directive';
+export * from './feature.guard';
+export * from './feature.service';
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,1 +1,2 @@
 export * from './components/relative-time.component';
+export * from './feature-management';
