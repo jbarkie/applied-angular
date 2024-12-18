219edc906c4fa734a2c1d6282e93b072d6e302e2
Joe Barkie <josephbarkie@hotmail.com>
2024-12-18 16:15:49 -0500

    Format date
    Use relative time component.

--- src/app/areas/lrc/components/list.component.ts
+++ src/app/areas/lrc/components/list.component.ts
@@ -1,19 +1,23 @@
 import { DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { PostsStore } from '../services/post-store';
+import { RelativeTimeComponent } from '@shared';
 
 @Component({
   selector: 'app-lrc-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [DatePipe],
+  imports: [DatePipe, RelativeTimeComponent],
   template: `
     <p>list of posts</p>
     @for (post of store.posts(); track post.id) {
-      <div>
+      <div class="p-12">
         <p>Name: {{ post.name }}</p>
         <p>Description: {{ post.description }}</p>
         <p>Posted By: {{ post.postedBy }}</p>
-        <p>Posted On: {{ post.datePosted | date: 'medium' }}</p>
+        <p>
+          Posted On: {{ post.datePosted | date: 'medium' }}
+          <app-relative-time [date]="post.datePosted" />
+        </p>
         <p>Link: {{ post.link }}</p>
       </div>
     }
