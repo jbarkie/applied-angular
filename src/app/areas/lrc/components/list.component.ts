782883a456242a63693b6ebf049942b6828c087f
Joe Barkie <josephbarkie@hotmail.com>
2024-12-18 15:00:05 -0500

    Display raw response

--- src/app/areas/lrc/components/list.component.ts
+++ src/app/areas/lrc/components/list.component.ts
@@ -1,10 +1,16 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, ChangeDetectionStrategy, resource } from '@angular/core';
 
 @Component({
   selector: 'app-lrc-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <p>list</p> `,
+  imports: [JsonPipe],
+  template: ` <p>list of posts</p>
+    <pre>{{ posts.value() | json }}</pre>`,
   styles: ``,
 })
-export class ListComponent {}
+export class ListComponent {
+  posts = resource({
+    loader: () => fetch('api/posts').then((p) => p.json()),
+  });
+}
