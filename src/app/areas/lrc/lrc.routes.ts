88e2ac0f5b5dabd96f24a2d78e14ec1a9509ae45
Joe Barkie <josephbarkie@hotmail.com>
2024-12-18 15:19:05 -0500

    Add provider
    For PostAPI

--- src/app/areas/lrc/lrc.routes.ts
+++ src/app/areas/lrc/lrc.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LrcComponent } from './lrc.component';
+import { PostApi } from './services/post-api';
 export const LRC_ROUTES: Routes = [
   {
     path: '',
+    providers: [PostApi],
     component: LrcComponent,
     children: [],
   },
