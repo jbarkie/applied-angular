32ae1e7937482ad28baeaf35ab550410834ab5b3
Joe Barkie <josephbarkie@hotmail.com>
2024-12-18 16:12:31 -0500

    Update component

--- src/app/areas/lrc/components/list.component.ts
+++ src/app/areas/lrc/components/list.component.ts
@@ -1,17 +1,25 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { PostApi } from '../services/post-api';
+import { PostsStore } from '../services/post-store';
 
 @Component({
   selector: 'app-lrc-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [AsyncPipe, JsonPipe],
-  template: ` <p>list of posts</p>
-    <pre>{{ posts$ | async | json }}</pre>`,
+  imports: [DatePipe],
+  template: `
+    <p>list of posts</p>
+    @for (post of store.posts(); track post.id) {
+      <div>
+        <p>Name: {{ post.name }}</p>
+        <p>Description: {{ post.description }}</p>
+        <p>Posted By: {{ post.postedBy }}</p>
+        <p>Posted On: {{ post.datePosted | date: 'medium' }}</p>
+        <p>Link: {{ post.link }}</p>
+      </div>
+    }
+  `,
   styles: ``,
 })
 export class ListComponent {
-  api = inject(PostApi);
-
-  posts$ = this.api.getPosts();
+  store = inject(PostsStore);
 }
