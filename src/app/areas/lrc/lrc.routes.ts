8d1a03dadd46564352623256997d25f1966dc443
Joe Barkie <josephbarkie@hotmail.com>
2024-12-18 16:12:13 -0500

    Provide store

--- src/app/areas/lrc/lrc.routes.ts
+++ src/app/areas/lrc/lrc.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LrcComponent } from './lrc.component';
 import { PostApi } from './services/post-api';
+import { PostsStore } from './services/post-store';
 export const LRC_ROUTES: Routes = [
   {
     path: '',
-    providers: [PostApi],
+    providers: [PostApi, PostsStore],
     component: LrcComponent,
     children: [],
   },
