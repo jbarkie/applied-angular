552f5bf8e563f436147ab03cc08277b687b73c80
Joe Barkie <josephbarkie@hotmail.com>
2024-12-19 11:31:48 -0500

    Update children routes

--- src/app/areas/lrc/lrc.routes.ts
+++ src/app/areas/lrc/lrc.routes.ts
@@ -2,11 +2,22 @@ import { Routes } from '@angular/router';
 import { LrcComponent } from './lrc.component';
 import { PostApi } from './services/post-api';
 import { PostsStore } from './services/post-store';
+import { DetailsComponent } from './components/details.component';
+import { OverviewComponent } from './pages/overview.component';
 export const LRC_ROUTES: Routes = [
   {
     path: '',
     providers: [PostApi, PostsStore],
     component: LrcComponent,
-    children: [],
+    children: [
+      {
+        path: '',
+        component: OverviewComponent,
+      },
+      {
+        path: 'details/:id',
+        component: DetailsComponent,
+      },
+    ],
   },
 ];
