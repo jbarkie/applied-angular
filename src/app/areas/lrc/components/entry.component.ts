ff6ddd947172a8ef614057d3e7e9168005e768ed
Joe Barkie <josephbarkie@hotmail.com>
2024-12-19 11:48:28 -0500

    Update template
    Add form

--- src/app/areas/lrc/components/entry.component.ts
+++ src/app/areas/lrc/components/entry.component.ts
@@ -4,7 +4,25 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-lrc-entry',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
-  template: ` <p>entry</p> `,
+  template: `<form>
+    <div class="form-control">
+      <label for="name" class="label">Name of Post</label>
+      <input name="name" type="text" class="input input-primary" />
+    </div>
+    <div class="form-control">
+      <label for="description" class="label">Description</label>
+      <textarea
+        name="description"
+        type="text"
+        class="input input-primary"
+      ></textarea>
+    </div>
+    <div class="form-control">
+      <label for="name" class="label">URL</label>
+      <input name="name" type="url" class="input input-primary" />
+    </div>
+    <button type="submit" class="btn btn-primary">Add Link</button>
+  </form>`,
   styles: ``,
 })
 export class EntryComponent {}
