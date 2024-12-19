594356741ac3a99176a9f78ca7ab58dfb9737ade
Joe Barkie <josephbarkie@hotmail.com>
2024-12-19 12:35:04 -0500

    Add method
    To make POST request when new post is added to list.

--- src/app/areas/lrc/services/post-api.ts
+++ src/app/areas/lrc/services/post-api.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { PostApiResponse } from '../types';
+import {
+  PostApiResponse,
+  PostApiResponseItem,
+  PostCreateModel,
+} from '../types';
 
 export class PostApi {
   #http = inject(HttpClient);
   getPosts() {
     return this.#http.get<PostApiResponse>('/api/posts');
+  }
+
+  addPost(post: PostCreateModel) {
+    return this.#http.post<PostApiResponseItem>('/api/posts', post);
   }
 }
